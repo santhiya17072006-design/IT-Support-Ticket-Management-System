9e4fdfcbd537e3f6de5eae3bbb4722901289a8bb
Sandhiya R <santhiya17072006@gmail.com>
2026-08-04 16:06:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "IT Support Ticket Management System"
+    return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route("/create-ticket")
+def create_ticket():
+    return render_template("create_ticket.html")
+
+@app.route("/admin")
+def admin():
+    return render_template("admin.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
